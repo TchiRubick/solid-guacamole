a63d75450c6b50c68d21ec79ee2d735276a2cf2a
Tophy Lad <tophyrakotomavo@gmail.com>
2024-11-29 22:49:50 +0300

    chore: register

--- next-app/src/actions/auth/signup.ts
+++ next-app/src/actions/auth/signup.ts
@@ -2,33 +2,18 @@
 
 import { hashPassword } from '@/lib/password';
 import { setSessionTokenCookie } from '@/lib/session-cookies';
+import { getScopedI18n } from '@/locales/server';
 import { createSession } from '@/models/session';
 import { checkExisting, create } from '@/models/user';
-import { z } from 'zod';
+import { signupSchema } from '@/validator/signup.validator';
+import type { z } from 'zod';
 
-export const signupSchema = z.object({
-  username: z
-    .string()
-    .min(3, 'Username must be at least 3 characters')
-    .max(31, 'Username must be less than 31 characters')
-    .regex(
-      /^[a-zA-Z0-9_-]+$/,
-      'Username can only contain letters, numbers, underscores, and dashes'
-    ),
-  email: z.string().email('Invalid email address').toLowerCase(),
-  password: z
-    .string()
-    .min(6, 'Password must be at least 6 characters')
-    .max(255, 'Password must be less than 255 characters'),
-  phone: z.string().min(10, 'Phone number must be at least 10 characters'),
-  address: z.string().min(5, 'Address must be at least 5 characters'),
-  city: z.string().min(4, 'City must be at least 4 characters'),
-  country: z.string().min(4, 'Country must be at least 4 characters'),
-  zipCode: z.string().min(2, 'Zip code must be at least 2 characters'),
-});
+type SigninInput = z.infer<ReturnType<typeof signupSchema>>;
 
-export const signup = async (input: z.infer<typeof signupSchema>) => {
-  const validatedInput = signupSchema.parse(input);
+export const signup = async (input: SigninInput) => {
+  const tError = await getScopedI18n('server-error');
+  const tSignup = await getScopedI18n('signup-form');
+  const validatedInput = signupSchema(tError).parse(input);
 
   const existingUser = await checkExisting(
     validatedInput.username,
@@ -36,7 +21,7 @@ export const signup = async (input: z.infer<typeof signupSchema>) => {
   );
 
   if (existingUser) {
-    throw 'User already exists';
+    throw tSignup('user-already-exists');
   }
 
   const passwordHash = await hashPassword(validatedInput.password);
@@ -47,7 +32,7 @@ export const signup = async (input: z.infer<typeof signupSchema>) => {
   });
 
   if (!user) {
-    throw 'Failed to create user';
+    throw tSignup('failed-to-create-user');
   }
 
   const session = await createSession(user.id);
