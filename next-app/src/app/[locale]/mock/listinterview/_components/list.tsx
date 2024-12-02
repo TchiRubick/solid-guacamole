6ef39c706e62213a3689f0efc08e5de830d5dd16
andersonsuiriex <andersonsuiriex@gmail.com>
2024-12-02 18:40:50 +0300

    chore: creaction listinterview

---
+++ next-app/src/app/[locale]/mock/listinterview/_components/list.tsx
@@ -0,0 +1,18 @@
+'use client'
+import { Button } from "@/components/ui/button";
+
+
+
+export const List = () => {
+    return (
+        <>
+        <div>
+            Interview List 
+            <Button>Create</Button> 
+
+        </div>
+        </>
+    )
+}
+
+export default List;
---
+++ next-app/src/app/[locale]/mock/listinterview/page.tsx
@@ -0,0 +1,13 @@
+import List from "./_components/list"
+
+
+const Listinterview = () => {
+    return (
+        <>
+        <List/>
+        </>
+    )
+    
+}
+
+export default Listinterview;
