b1179e2f7649b8a8ac9724278c985c31a5ce2450
Tophy Lad <tophyrakotomavo@gmail.com>
2024-12-13 23:45:46 +0300

    chore: mock candidate test web cam

--- next-app/src/app/[locale]/mock/invitation/[token]/page.tsx
+++ next-app/src/app/[locale]/mock/invitation/[token]/page.tsx
@@ -1,9 +1,23 @@
 import { verifTokenStatusQuery } from '@/actions/interview/verif-token-status';
+import { Recorder } from '../../interview/_components/recorder';
+import { Button } from '@/components/ui/button';
 
 const Page = async ({ params }: { params: Promise<{ token: string }> }) => {
   const interview = await verifTokenStatusQuery((await params).token);
 
-  return <div>{interview ? 'ok' : 'ko'}</div>;
+  return (
+    <div>
+      {interview ? (
+        <div>
+          <span>Test your camera and microphone</span>
+          <Recorder />
+          <Button>Start Interview</Button>
+        </div>
+      ) : (
+        'Your interview has already expired'
+      )}
+    </div>
+  );
 };
 
 export default Page;
