127286424fb71909b431a4157907d4699a9ed5ef
Aro Fortunat <aro.fortunat.ralaiseheno@esti.mg>
2024-12-19 18:43:05 +0300

    errorPage invitation

--- next-app/src/app/[locale]/invitation/error.tsx
+++ next-app/src/app/[locale]/invitation/error.tsx
@@ -1,11 +1,17 @@
 'use client';
-import { NotAuthorized } from '@/components/not-authorized';
+import { CheckCircle } from 'lucide-react';
 
 const ErrorPage = ({ error }: { error: Error & { digest?: string } }) => {
   return (
-    <main className='flex h-full flex-col items-center justify-center'>
-      <NotAuthorized error={error.message} />
-    </main>
+    <div className='flex min-h-screen flex-col items-center justify-center bg-background'>
+      <div className='space-y-6 text-center'>
+        <CheckCircle className='mx-auto h-24 w-24 text-destructive' />
+        <h1 className='text-4xl font-bold tracking-tight'>{error}</h1>
+        <p className='max-w-md text-xl text-muted-foreground'>
+          Sorry, something went wrong !
+        </p>
+      </div>
+    </div>
   );
 };
 
